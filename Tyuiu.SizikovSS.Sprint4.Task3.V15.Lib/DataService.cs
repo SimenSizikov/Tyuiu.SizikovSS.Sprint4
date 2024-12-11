0e2a3212860b9bd49ca3c4c29265636b7d36e0ba
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-12-11 14:14:56 +0500

    Task3

--- Tyuiu.SizikovSS.Sprint4.Task3.V15.Lib/DataService.cs
+++ Tyuiu.SizikovSS.Sprint4.Task3.V15.Lib/DataService.cs
@@ -9,7 +9,8 @@ namespace Tyuiu.SizikovSS.Sprint4.Task3.V15.Lib
             int count = 0;
             for (int i = 0; i < array.GetLength(0); i++)
             {
-                if (array[i, array.GetLength(1) - 1] % 2 != 0) count++;
+                if (array[i, array.GetLength(1) - 1] % 2 != 0)
+                    count++;
             }
             return count;
         }
