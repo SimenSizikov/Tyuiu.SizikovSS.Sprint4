0bcf9611464a2695ed5dcb3c037cbf86ce32ebb8
Сизиков Семён <Tyuiu.SizikovSS@outlook.com>
2024-12-13 22:39:07 +0500

    Task4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-            int[,] array = new int[,] { { -1, 2,  3 }, {  4, -5,  6}, { 7,-8 , 9 } };
+            int[,] array = new int[,] { { 2, 4, 6, 8 }, { 1, 3, 5, 7 }, { 9, 2, 4, 6 }, { 8, 1, 3, 5 } };
             int s = 0;
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    if (array[i, j] % 2 == 0)
+                    if (array[i, j] % 2 != 0)
                     {
-                        s += array[i, j];
+                        s++;
                     }
                 }
             }
---
+++ Tyuiu.SizikovSS.Sprint4.Task4.V6/Program.cs
@@ -0,0 +1,74 @@
+﻿using Tyuiu.SizikovSS.Sprint4.Task4.V6.Lib;
+
+namespace Tyuiu.SizikovSS.Sprint4.Task4.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new();
+
+            Console.Title = "Спринт #4 | Выполнил: Сизиков С. С. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Сизиков С. С. | РППб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 4 до 9.                          *");
+            Console.WriteLine("* Заменить нечетные элементы массива на 0.                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mass = new int[5, 5];
+
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    do
+                    {
+                        Console.Write($"Введите [{i},{j}] элемент массива: ");
+                        mass[i, j] = Convert.ToInt32(Console.ReadLine());
+                        if (mass[i, j] < 4 || mass[i, j] > 9) Console.WriteLine("Элементы массива должны быть в данном диапазоне[5,9].");
+                    } while (mass[i, j] < 4 || mass[i, j] > 9);
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    Console.Write(mass[i, j] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+            mass = ds.Calculate(mass);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Полученный массив:");
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    Console.Write(mass[i, j] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
